658a55e3baf3be2ae30fdec8a554dd679893a9ad
ayaneey <ayan.h18@hotmail.com>
2022-10-22 20:12:51 +0100

    created seeds for all models

--- seeds/index.js
+++ seeds/index.js
@@ -3,8 +3,14 @@ const userData = require("../seeds/userSeeds");
 const commentData = require("../seeds/commentSeeds");
 const blogData = require("../seeds/blogSeeds");
 
-const seedAllData = async ()=>{
-    sequelize.sync({
-        
-    })         
-}
+const seedAllData = async () => {
+  await sequelize.sync({
+    force: true, //commands sequelize to populate new data and override old data.
+  });
+
+  await userData();
+  await blogData();
+  await commentData();
+};
+
+seedAllData();
