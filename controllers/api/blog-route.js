25f85b4618976a94fbc4b1e3c11f9fe59d6ad83c
ayaneey <ayan.h18@hotmail.com>
2022-10-22 21:46:18 +0100

    set up seeds

--- controllers/api/blog-route.js
+++ controllers/api/blog-route.js
@@ -1,4 +1,4 @@
-const router = require("express-router");
+const router = require("express").Router();
 const Blog = require("../../models/User");
 
 module.exports = router;
--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,1 +1,16 @@
 const router = require("express").Router();
+
+const userRoutes = require("./user-routes");
+
+const blogRoutes = require("./blog-routes");
+
+const commentRoutes = require("./comment-route");
+
+const authRoute = require("./auth-route");
+
+router.use("/users", userRoutes);
+router.use("/blogs", blogRoutes);
+router.use("/comment", commentRoutes);
+router.use("/auth", authRoute);
+
+module.exports = router;
