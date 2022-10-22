4a1b7b587267facb12e0a5c0d9a189d75bbb95ef
ayaneey <ayan.h18@hotmail.com>
2022-10-22 18:52:22 +0100

    added helper.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const path = require("path");
 const express = require("express");
 const session = require("express-session");
 const exphbs = require("express-handlebars");
+
 // Initializes Sequelize with session store
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
