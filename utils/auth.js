8a17a59a044896caea0053f00b1e6afabe044d2c
Ayan Ahmed <ayanahmed@Ayans-MacBook-Pro.local>
2022-11-10 21:11:52 +0000

    fixed errors preventing server from starting

--- utils/auth.js
+++ utils/auth.js
@@ -1,9 +1,11 @@
 const withAuth = (req, res, next) => {
-  // If user is not logged in, re-direct them to login page
-  if (!req.session.loggedIn) {
-    res.redirect("/login");
-  } else {
-    // Therefore, if user is logged in, execute the route function that will allow them to view the posts
-    next(); // We call next() only if the user is authenticated
-  }
+	// If user is not logged in, re-direct them to login page
+	if (!req.session.loggedIn) {
+		res.redirect("/login");
+	} else {
+		// Therefore, if user is logged in, execute the route function that will allow them to view the posts
+		next(); // We call next() only if the user is authenticated
+	}
 };
+
+module.exports = withAuth;
