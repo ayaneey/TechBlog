bf2c1e0491ffeed5f3fadb1b0f35ceff8f5d8139
Ayan Ahmed <ayanahmed@macbook-pro.home>
2022-11-11 22:20:31 +0000

    fix model files

--- models/Comment.js
+++ models/Comment.js
@@ -7,41 +7,41 @@ const Post = require("./Post");
 class Comment extends Model {}
 
 Comment.init(
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    comment_text: {
-      type: DataTypes.TEXT,
-      require: true,
-    },
-    user_id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      references: {
-        model: User,
-        key: "id",
-      },
-    },
-    post_id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      references: {
-        model: Post,
-        key: "id",
-      },
-    },
-  },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: "comments",
-  }
+	{
+		id: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			primaryKey: true,
+			autoIncrement: true,
+		},
+		comment_text: {
+			type: DataTypes.STRING,
+			require: true,
+		},
+		user_id: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			references: {
+				model: User,
+				key: "id",
+			},
+		},
+		post_id: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			references: {
+				model: Post,
+				key: "id",
+			},
+		},
+	},
+	{
+		sequelize,
+		timestamps: false,
+		freezeTableName: true,
+		underscored: true,
+		modelName: "comment",
+	}
 );
 
 module.exports = Comment;
--- models/index.js
+++ models/index.js
@@ -3,30 +3,29 @@ const Comment = require("./Comment");
 const Post = require("./Post");
 
 User.hasMany(Post, {
-  foreignKey: "user_id",
+	foreignKey: "user_id",
 });
 
 User.hasMany(Comment, {
-  foreignKey: "user_id",
-  onDelete: "CASCADE",
+	foreignKey: "user_id",
 });
 
 Comment.belongsTo(User, {
-  foreignKey: "user_id",
+	foreignKey: "user_id",
 });
 
 Comment.belongsTo(Post, {
-  foreignKey: "post_id",
+	foreignKey: "post_id",
 });
 
 Post.belongsTo(User, {
-  foreignKey: "user_id",
-  onDelete: "CASCADE",
+	foreignKey: "user_id",
+	onDelete: "CASCADE",
 });
 
 Post.hasMany(Comment, {
-  foreignKey: "post_id",
-  onDelete: "CASCADE",
+	foreignKey: "post_id",
+	onDelete: "CASCADE",
 });
 
 module.exports = { User, Comment, Post };
