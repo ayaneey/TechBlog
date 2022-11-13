66332f741279f764b7b335c28fed1e74470141a5
Ayan Ahmed <ayanahmed@macbook-pro.home>
2022-11-13 12:08:39 +0000

    made amendments to post route to debug errors

--- controllers/api/post-route.js
+++ controllers/api/post-route.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const Post = require("../../models/Post");
+const { User, Post, Comment } = require("../../models");
 
 // Find posts
 router.get("/", async (req, res) => {
