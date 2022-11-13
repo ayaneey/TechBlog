43e32213c9d626fd068243b70981c6af4737ff20
Ayan Ahmed <ayanahmed@macbook-pro.home>
2022-11-13 21:11:52 +0000

    tested comment route in Postman - slightly amended seeds

--- seeds/commentSeeds.js
+++ seeds/commentSeeds.js
@@ -2,16 +2,18 @@ const Comment = require("../models/Comment");
 
 const seedData = [
 	{
-		username: "Ayan",
+		comment_text: "hello",
+		user_id: 1,
+		post_id: 2,
 		text_content: "this is comment 1",
 		date: "10/22/2022",
-		user_id: 1,
 	},
 	{
-		username: "Alex",
-		text_content: "this is comment 2",
-		date: "10/22/2022",
-		user_id: 2, // the id of the user should be between 1-maximum number of users added in the seed data
+		// username: "Alex",
+		// text_content: "this is comment 2",
+		// date: "10/22/2022",
+		// user_id: 2, // the id of the user should be between 1-maximum number of users added in the seed data
+		// post_id: 1, // the id of the blog should be between 1-maximum number of blogs added in the seed data
 	},
 ];
 
