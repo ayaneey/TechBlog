d7687abda9ec2c291ec010ea3bb49a4b8e7c2b92
Ayan Ahmed <ayanahmed@macbook-pro.home>
2022-11-15 23:08:33 +0000

    made amendments to delete.js

--- public/js/delete.js
+++ public/js/delete.js
@@ -5,9 +5,11 @@ const delButtonHandler = async (event) => {
 		window.location.toString("/")[
 			window.location.toString().split("/").length - 1
 		];
+	// Send a POST request to the API endpoint
 	const response = await fetch(`/api/posts/${id}`, {
 		method: "DELETE",
 	});
+	// If successful, redirect the browser to the dashboard page
 	if (response.ok) {
 		document.location.replace("/dashboard");
 	} else {
---
+++ public/js/signup.js
@@ -0,0 +1,30 @@
+const e = require("express");
+
+const signupFormHandler = async (event) => {
+	event.preventDefault();
+
+	// Collect values from the login form
+	const username = document.querySelector("#username-signup").value.trim();
+	const email = document.querySelector("#email-signup").value.trim();
+	const password = document.querySelector("#password-signup").value.trim();
+
+	if (username && email && password) {
+		// Send a POST request to the API endpoint
+		const response = await fetch("/api/users", {
+			method: "POST",
+			body: JSON.stringify({ username, email, password }),
+			headers: { "Content-Type": "application/json" },
+		});
+
+		// If successful, redirect the browser to the dashboard page
+		if (response.ok) {
+			document.location.replace("/dashboard");
+		} else {
+			alert(response.statusText);
+		}
+	}
+};
+
+document
+	.querySelector(".signup-form")
+	.addEventListener("submit", signupFormHandler);
