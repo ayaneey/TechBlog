a8d4666f2ac89ec4392fa342f9c6fae4df4cc9f1
Ayan Ahmed <ayanahmed@ayans-mbp.home>
2022-11-20 23:51:04 +0000

    fixed seeding error

--- seeds/index.js
+++ seeds/index.js
@@ -4,7 +4,7 @@ const commentData = require("../seeds/commentSeeds");
 const blogData = require("../seeds/blogSeeds");
 
 const seedAllData = async () => {
-	await sequelize.sync({ force: false }); //commands sequelize to populate new data and override old data.
+	await sequelize.sync({ force: true }); //commands sequelize to populate new data and override old data.
 	await userData();
 	await blogData();
 	await commentData();
