320a59abfe11d8f9347ea7096df2e08cc54ea955
Ayan Ahmed <ayanahmed@Ayans-MacBook-Pro.local>
2022-11-21 23:06:50 +0000

    made some changes

--- public/js/login.js
+++ public/js/login.js
@@ -23,32 +23,32 @@ const loginFormHandler = async (event) => {
 	}
 };
 
-const signupFormHandler = async (event) => {
-	event.preventDefault();
+// const signupFormHandler = async (event) => {
+// 	event.preventDefault();
 
-	const username = document.querySelector("#username-signup").value.trim();
-	const email = document.querySelector("#email-signup").value.trim();
-	const password = document.querySelector("#password-signup").value.trim();
+// 	const username = document.querySelector("#username-signup").value.trim();
+// 	const email = document.querySelector("#email-signup").value.trim();
+// 	const password = document.querySelector("#password-signup").value.trim();
 
-	if (username && email && password) {
-		const response = await fetch("/api/users/signup", {
-			method: "POST",
-			body: JSON.stringify({ username, email, password }),
-			headers: { "Content-Type": "application/json" },
-		});
+// 	if (username && email && password) {
+// 		const response = await fetch("/api/users/signup", {
+// 			method: "POST",
+// 			body: JSON.stringify({ username, email, password }),
+// 			headers: { "Content-Type": "application/json" },
+// 		});
 
-		if (response.ok) {
-			document.location.replace("/dashboard");
-		} else {
-			alert(response.statusText);
-		}
-	}
-};
+// 		if (response.ok) {
+// 			document.location.replace("/dashboard");
+// 		} else {
+// 			alert(response.statusText);
+// 		}
+// 	}
+// };
 
 document
 	.querySelector(".login-form")
 	.addEventListener("submit", loginFormHandler);
 
-document
-	.querySelector(".signup-form")
-	.addEventListener("submit", signupFormHandler);
+// document
+// 	.querySelector(".signup-form")
+// 	.addEventListener("submit", signupFormHandler);
