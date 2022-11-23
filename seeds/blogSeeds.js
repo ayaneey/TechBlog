16a9c062d838a5e40200e8a2c8df7ff01667f559
Ayan Ahmed <ayanahmed@ayans-mbp.home>
2022-11-23 22:02:29 +0000

    added some styling to seeded posts

--- seeds/blogSeeds.js
+++ seeds/blogSeeds.js
@@ -2,24 +2,22 @@ const Post = require("../models/Post");
 
 const seedData = [
 	{
-		title: "Blog1",
-		text_content: "this is text_content 1",
+		title: "Why MVC is so important",
+		text_content:
+			"MVC allows developers to maintain a true separation of concerns, devising their code between the Model layer for data, the View layer for design, and the Controller layer for application logic.",
 		user_id: 1,
 	},
 	{
-		title: "Blog2",
-		text_content: "this is text_content 2",
+		title: "Authentication vs. Authorization",
+		text_content:
+			"There is a difference between authentication and authorization. Authentication means confirming your own identity, whereas authorization means being allowed access to the system. ",
 		user_id: 2,
 	},
 	{
-		title: "Blog4",
-		text_content: "this is text_content 2",
+		title: "Object-Relational Mapping",
+		text_content:
+			"I have really enjoyed learning about ORM. It's really simplified the way I create queries in SQL",
 		user_id: 1,
-	},
-	{
-		title: "Blog5",
-		text_content: "this is text_content 2",
-		user_id: 2,
 	},
 ];
 
